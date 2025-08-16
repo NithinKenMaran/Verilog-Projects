b63f2505ef5d63d3809583cd0a411f43b0cf32fd
KennyTit <nithinmaran@gmail.com>
2025-08-16 18:56:00 +0530

    reogranized root structure

--- CPU - 16 bit - Pipelined/accumulator.v
+++
@@ -1,9 +0,0 @@
-module accumulator(
-    input clk, load_en, 
-    input [15:0] data_in,
-    output reg [15:0] data_out
-);
-    always @ (posedge clk) begin
-        data_out <= load_en ? data_in : data_out;   
-    end
-endmodule
--- CPU - 16 bit - Pipelined/header/opcodes.vh
+++
@@ -1,9 +0,0 @@
-`define ADD 4'b0000
-`define SUB 4'b0001
-`define AND 4'b0010
-`define OR 4'b0011
-`define XOR 4'b0100
-
-`define LDR 4'b0101
-`define LPM 4'b0110
-`define STM 4'b0111
